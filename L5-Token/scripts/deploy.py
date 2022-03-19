9c2dd52a1a84cea69bcd0c3721f115577fd11440
K1-R1 <kyranrawlinson@gmail.com>
2022-03-19 14:48:26 +0000

    Reorganised repo

--- L5-Token/scripts/deploy.py
+++
@@ -1,8 +0,0 @@
-from brownie import config, accounts, HackToken
-
-def main():
-    deploy()
-
-def deploy():
-    account = accounts.add(config['wallets']['dev_account_1']['private_key'])
-    hack_token = HackToken.deploy({'from':account}, publish_source=True)
