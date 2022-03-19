e300323144a179a5f9386fa50a703f95d60d90fd
K1-R1 <kyranrawlinson@gmail.com>
2022-03-19 16:11:35 +0000

    L7: Added deployment scripts

---
+++ L7-Force/scripts/deploy.py
@@ -0,0 +1,13 @@
+from brownie import config, accounts, HackForce
+
+def main():
+    deploy()
+
+def deploy():
+    account = accounts.add(config['wallets']['dev_account_1']['private_key'])
+    hack_force = HackForce.deploy({'from':account}, publish_source=True)
+    self_destruct(account, hack_force)
+
+def self_destruct(account, hack_force):
+    hack_force._receive({'from': account, 'value':( 0.001 * 10**18)}).wait(1)
+    hack_force.selfDestruct({'from': account}).wait(1)
