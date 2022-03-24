0564c7f442c1828d78203540d738fb1301045b8c
K1-R1 <kyranrawlinson@gmail.com>
2022-03-24 17:56:45 +0000

    L18: Updated config

--- L18-MagicNumber/hardhat.config.js
+++ L18-MagicNumber/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -13,9 +14,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+require('dotenv').config()
+const PK1 = process.env.PK1
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const ETHERSCAN_API = process.env.ETHERSCAN_API
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.6.0",
+  defaultNetwork: "rinkeby",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PK1]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API
+  }
 };
